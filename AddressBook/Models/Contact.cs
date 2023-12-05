14dfa56108f18bcd754ae9567f3abc1b0c0b01f6
Anna <nakupanda89@gmail.com>
2023-12-05 14:10:48 +0100

    Created a menu and contactService

--- AddressBook/Models/Contact.cs
+++ AddressBook/Models/Contact.cs
@@ -1,5 +1,14 @@
-﻿namespace AddressBook.Models;
+﻿using AddressBook.Interfaces;
 
-internal class Contact
+namespace AddressBook.Models;
+
+public class Contact : IContact
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int PhoneNumber { get; set; }
+    public string Address { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string ZipCode { get; set; } = null!;
 }
---
+++ AddressBook/Services/MenuService.cs
@@ -0,0 +1,116 @@
+﻿using AddressBook.Interfaces;
+using AddressBook.Models;
+
+namespace AddressBook.Services;
+
+public class MenuService : IMenuService
+{
+    private readonly IContactService _contactService = new ContactService();
+    public void ShowMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("Your Address Book");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Remove Contact");
+            Console.WriteLine("3. Get Single Contact");
+            Console.WriteLine("4. Get All Contacts");
+            Console.WriteLine("5. Close Application");
+            Console.WriteLine();
+            Console.WriteLine("Choose an option: ");
+
+            var option = Console.ReadLine();
+
+
+            switch (option)
+            {
+                case "1":
+                    AddContactOptions();
+                    break;
+                case "2":
+                    RemoveContactOptions();
+                    break;
+                case "3":
+                    GetSingleContactOptions();
+                    break;
+                case "4":
+                    GetAllContactsOptions();
+                    break;
+                case "5":
+                    CloseApplicationOptions();
+                    return;
+
+            }
+        }
+    }
+
+    private void AddContactOptions()
+    {
+        IContact contact = new Contact();
+
+        Console.WriteLine("Add Contact");
+        Console.WriteLine("Add a firstname: ");
+        contact.FirstName = Console.ReadLine()!;
+
+        Console.WriteLine("Add a lastname: ");
+        contact.LastName = Console.ReadLine()!;
+
+        Console.WriteLine("Add a email: ");
+        contact.Email = Console.ReadLine()!;
+
+        Console.WriteLine("Add a phonenumber: ");
+        contact.PhoneNumber = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Add a address: ");
+        contact.Address = Console.ReadLine()!;
+
+        Console.WriteLine("Add a city: ");
+        contact.City = Console.ReadLine()!;
+
+        Console.WriteLine("Add a zipcode: ");
+        contact.ZipCode = Console.ReadLine()!;
+
+        //var res = _contactService.AddContact(contact);
+    }
+
+    private void RemoveContactOptions()
+    {
+        Console.WriteLine("Remove Contact");
+        Console.WriteLine("Enter the email of the user you want to remove: ");
+        var email = Console.ReadLine();
+        
+    }
+
+    private void GetSingleContactOptions()
+    {
+        Console.WriteLine("Get Single Contact");
+        Console.WriteLine("");
+    }
+
+    private void GetAllContactsOptions()
+    {
+        var res = _contactService.GetAllContacts();
+
+        Console.WriteLine("Get All Contacts");
+        Console.WriteLine("");
+
+        if (!res.Any())
+        {
+            Console.WriteLine("No contacts found");
+        }
+        else
+        {
+            foreach (var contact in res)
+            {
+                Console.WriteLine($"{contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"{contact.Email} {contact.PhoneNumber}");
+                Console.WriteLine($"{contact.Address} {contact.ZipCode} {contact.City}");
+            }
+        }
+    }
+
+    private void CloseApplicationOptions()
+    {
+        Environment.Exit(0);
+    }
+}
