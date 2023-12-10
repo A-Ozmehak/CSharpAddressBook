54ae04841c4a68bd23f64c7808d8c840332f0f1b
Anna <nakupanda89@gmail.com>
2023-12-10 17:03:24 +0100

    added comments

--- Shared/Interfaces/IContactService.cs
+++ Shared/Interfaces/IContactService.cs
@@ -3,28 +3,29 @@
 public interface IContactService
 {
     /// <summary>
-    /// 
+    /// Add a contact
     /// </summary>
-    /// <param name="contact"></param>
+    /// <param name="contact">The contact to be added</param>
+    /// <returns>True if the contact was added successfully, false otherwise</returns>
     bool AddContact(IContact contact);
 
     /// <summary>
-    /// 
+    /// Remove a contact
     /// </summary>
-    /// <param name="email"></param>
+    /// <param name="email">Removes a contact by email</param>
+    /// <returns>The contact if found, null otherwise</returns>
     bool RemoveContactByEmail(string email);
 
     /// <summary>
-    /// 
+    /// Get one contact 
     /// </summary>
-    /// <param name="FirstName"></param>
-    /// <returns></returns>
+    /// <param name="FirstName">Find a contact by entering a firstname</param>
+    /// <returns>Shows the contact information otherwise null</returns>
     IContact GetSingleContact(string FirstName);
 
     /// <summary>
-    /// 
+    /// Get all contacts
     /// </summary>
-    /// <returns></returns>
-
+    /// <returns>Show all contacts otherwise null</returns>
     IEnumerable<IContact> GetAllContacts();
 }
