10cbf34c003e564d9aed36cfdb638bd8598ca424
Anna <nakupanda89@gmail.com>
2023-12-15 18:42:59 +0100

    made tests for consol application

---
+++ AddressBook.Tests/FileService_Tests.cs
@@ -0,0 +1,37 @@
+﻿using Shared.Interfaces;
+using Shared.Services;
+
+namespace AddressBook.Tests;
+
+public class FileService_Tests
+{
+    [Fact]
+    public void SaveContactToFileShould_SaveAContactToTheFile_ThenReturnTrue()
+    {
+        // Arrange
+        IFileService fileService = new FileService();
+        string filePath = @"C:\Users\Anna\Documents\Repos\CSharp\CSharpAddressBook\addressBookContacts.json";
+        string content = "Test content";
+
+        // Act
+        bool result = fileService.SaveContactToFile(filePath, content);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void GetContactsFromFileShould_ReturnTrue_IfTheFileExists()
+    {
+        // Arrange
+        IFileService fileService = new FileService();
+        string filePath = @"C:\Users\Anna\Documents\Repos\CSharp\CSharpAddressBook\addressBookContacts.json";
+        string content = "Test content";
+
+        // Act
+        bool result = fileService.SaveContactToFile(filePath, content);
+
+        // Assert
+        Assert.True(result);
+    }
+}
